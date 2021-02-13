77fff2209177aae8154930c0328fca10c83064fb
netaro-N <takebbb1+github@gmail.com>
2021-02-13 22:18:43 +0900

    hover

---
+++ DesignComponent/Js&jQuery/Progate/hover/hover.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.item').hover(
+    function() {
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
+})
