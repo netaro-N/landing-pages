71abf62313e5e21d4717f56b5e0bf4c1f7669777
netaro-N <takebbb1+github@gmail.com>
2021-08-11 13:23:42 +0900

    resize

--- testFile/responsiveFlower/step1-2.js
+++ testFile/responsiveFlower/step1-2.js
@@ -3,23 +3,24 @@ var container = document.getElementById("canvas-container");
 var ctx = canvas.getContext("2d");
 var imgCnt = 25;  // 描画する画像の数
 var aryImg = [];  // 画像の情報を格納
-// 画像用の親要素幅高取得 clientWidth要勉
-var conw = container.clientWidth;   // 【可変】親要素の幅
-var conh = container.clientHeight;   // 【準固定】親要素の高さ(ここもビューポートによってレスポンシブなのでリサイズ時に常に取得)
-// キャンバス幅と高さを初期設定 キャンバスの幅canvas.width要勉
-canvas.width = conw;
-canvas.height = conh;
 
+
+// 画像用の親要素幅高取得 clientWidth要勉
+// キャンバス幅と高さを初期設定 キャンバスの幅canvas.width要勉
+canvas.width = container.clientWidth;   // 【可変】親要素の幅
+canvas.height = container.clientHeight;   // 【準固定】親要素の高さ(ここもビューポートによってレスポンシブなのでリサイズ時に常に取得)
 var cvsw = canvas.width;   // キャンバスの幅canvas.width要勉
 var cvsh = canvas.height;   // キャンバスの高さ
+var imgBaseSizeW = 15/1000*cvsw;    // 画像の基本サイズ横幅(1000のとき15)
+var imgBaseSizeH = imgBaseSizeW*1.25;  // 画像の基本サイズ立幅(幅1000のとき18.5)
 
+
+// 桜が落ちる速度
 var dropspeed = 10;
 if (cvsh<500){
   dropspeed = 15;
 }
 
-var imgBaseSizeW = 15/1000*conw;    // 画像の基本サイズ横幅(1000のとき15)
-var imgBaseSizeH = imgBaseSizeW*1.25;  // 画像の基本サイズ立幅(幅1000のとき18.5)
 
 // 画像の読み込み
 var img = new Image();
@@ -56,4 +57,17 @@ function flow_start(){
   console.log(dropspeed);
   setImagas();
   setInterval(flow,dropspeed);
+}
+
+// リサイズ時
+window.onresize = function(){
+
+  var cvsw = container.clientWidth;   
+  var imgBaseSizeW = 15/1000*cvsw;
+  var imgBaseSizeH = imgBaseSizeW*1.25;
+  for(idx = 0;idx < imgCnt;idx++){
+    aryImg[idx].sizew = imgBaseSizeW;
+    aryImg[idx].sizeh = imgBaseSizeH;
+  }
+
 }
