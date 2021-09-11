98d053d97d8c02c0cdf228517ed88791a6a00032
netaro-N <takebbb1+github@gmail.com>
2021-09-11 11:00:47 +0900

    test

---
+++ Portfolio/assets/js/header.js
@@ -0,0 +1,34 @@
+// window.addEventListener("scroll", function () {
+//   var header = document.querySelector("header");
+//   var logo = document.querySelector("header p");
+//   var li = document.querySelector("header ul");
+//   header.classList.toggle("scroll-nav", window.scrollY > 0);
+//   logo.classList.toggle("xs", window.scrollY > 0);
+//   li.classList.toggle("xs", window.scrollY > 0);
+// });
+'use strict';
+
+{
+  // ハンバーガー
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+
+//ヘッダーアニメーション
+  window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("scroll-nav", window.scrollY > 0);
+    close.classList.toggle("scroll-nav", window.scrollY > 0);
+  });
+}
